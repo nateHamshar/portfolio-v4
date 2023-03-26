ca443bb9c54f31d1ec10c9dfba7228ecd1d91ddf
Nate <williamhamshar@gmail.com>
2023-03-25 20:28:57 -0400

    project component, some tweaks to about section

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import "../styles/home.css";
 import codeImage from "../images/code.svg"
+import ProjectCard from "./ProjectCard";
 const Home = () => {
     return (
         <div className="home">
@@ -7,7 +8,7 @@ const Home = () => {
                     <div className="landingText">
                         <h1 className="welcomeHeader">Hey, I'm <span className="accent name">Nate</span></h1>
                         <h2 className="welcomeSubheader">and I am a <span className="accent">front-end</span> web dev</h2>
-                        <button className="projectsButton">Check Out My Projects</button>
+                        <a className="projectsLink" href="#projects" >Check Out My Projects</a>
                     </div>
                     <img alt="" className="landingImage" src={codeImage} />
             </section>
@@ -15,11 +16,11 @@ const Home = () => {
             <section className="aboutSection">
                 <div className="aboutText">
                     <h1 className="aboutHeader">About Me</h1>
-                    <p className="aboutParagraph firstParagraph">I am a passionate front-end developer with a drive to build clean, modern, and user friendly websites and web apps that provide a great user experience. I have some full-stack experience, however, I prefer to focus on the front-end. I am most comfortable when building within the MERN stack, but I am always eager to learn something new. </p>
-                    <p className="aboutParagraph secondParagraph">As a self-taught developer, I have had to hold myself accountable for learning the next thing. With that being said, I have a lot of experience reading new documentation and implementing additional technologies into my projects. </p>
+                    <p className="aboutParagraph firstParagraph">I am a passionate front-end developer with a drive to build <span className="emphasis">clean</span>, <span className="emphasis">modern</span>, and <span className="emphasis">user-friendly</span> websites and web apps that provide a great user experience. I have some full-stack experience, however, I am more specialized in the front-end of projects. I am most comfortable when building within the <span className="emphasis">MERN stack</span>, but I am always eager to learn something new. </p>
+                    <p className="aboutParagraph secondParagraph">As a self-taught developer, I have had to hold myself accountable for learning new technologies and best practices. Therefore, I have quite a bit of <span className="emphasis">problem solving</span> and <span className="emphasis">adaptive learning</span> experience with the help of reading documentation and asking questions on communities like Stack Overflow and Github. And now that I mentioned it, you should probably check out <a href="https://github.com/natehamshar" className="githubLink" target="_blank" rel="noreferrer">my GitHub!</a></p>
                 </div>
                 <div className="techSkills">
-                    <h1 className="techSkillsHeader">My Technical Skils</h1>
+                    <h1 className="techSkillsHeader">My Technical Skills</h1>
                     <div className="techLists">
                         <div className="frontend">
                             <h1 className="frontendHeader">Front-End:</h1>
@@ -60,11 +61,26 @@ const Home = () => {
                 </div>
             </section>
 
-            <section className="projectsSection">
-                <div className="project">
-                    <h1>This for sizing next thing</h1>
-                </div>
+            <section className="projectsSection" id="projects">
+                
+                <h1 className="projectsHeader">My Projects</h1>
+
+                <ProjectCard title={"Ticket Management System"}
+                             imageSource={require("../images/ticket-image.jpeg")}
+                             projectDescription={"This is a full-stack help ticket system built with React on the front-end, Express and Node for the server side, and Mongo for the Database. The user authorization and authentication is all handled with JSON Web Tokens to provide additional security to the application. The client side of this app is hosted on Netlify, and the server side is hosted on Render, so it may take a few seconds for them to both spin up. I have included a demo user button on the login page so you can easily test out the functionality for yourself. Feel free to add a ticket to test it out!"}
+                             codeURL={"https://github.com/nateHamshar/ticket-frontend"}
+                             liveURL={"https://ticketmanagingsystem.netlify.app/"}
+                    />
+                    <ProjectCard title={"Ticket Management System"}
+                             imageSource={require("../images/ticket-image.jpeg")}
+                             projectDescription={"This is a full-stack help ticket system built with React on the front-end, Express and Node for the server side, and Mongo for the Database. The user authorization and authentication is all handled with JSON Web Tokens to provide additional security to the application. The client side of this app is hosted on Netlify, and the server side is hosted on Render, so it may take a few seconds for them to both spin up. I have included a demo user button on the login page so you can easily test out the functionality for yourself. Feel free to add a ticket to test it out!"}
+                             codeURL={"https://github.com/nateHamshar/ticket-frontend"}
+                             liveURL={"https://ticketmanagingsystem.netlify.app/"}
+                    />
             </section>
+
+            <footer className="footer">
+            </footer>
         </div>
     )
 }
---
+++ src/components/ProjectCard.js
@@ -0,0 +1,30 @@
+import "../styles/projectCard.css"
+
+const ProjectCard = ({ title, imageSource, projectDescription, codeURL, liveURL }) => {
+    return (
+        <div className="projectCard">
+            <div className="imageDiv">
+                <img src={imageSource} alt="" className="projectImage" />
+            </div>
+            <div className="textDiv">
+                <h1 className="projectTitle">{title}</h1>
+                <p className="projectDescription">{ projectDescription }</p>
+                
+                <div className="linksDiv">
+                    <a href={codeURL}
+                    target="_blank"
+                    rel="noreferrer"
+                    className="codeLink">See The Code</a>
+                    <p className="or">- or -</p>
+                    <a href={liveURL}
+                    target="_blank"
+                    rel="noreferrer"
+                    className="liveLink">View The Live Site</a>                
+                </div>
+            </div>
+            
+        </div>
+    );
+}
+ 
+export default ProjectCard;
